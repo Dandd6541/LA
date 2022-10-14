d144bd2c3194ae3b8fd605631c8fd0d2be19e74d
Dandd6541 <Mordkid4@gmail.com>
2022-10-13 19:32:23 -0700

    about and base html

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# Add the include function to the import
+from . import views 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # In this case '' represents the root route
+    path('about/', views.about, name='about'),
+]
